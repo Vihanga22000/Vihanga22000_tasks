52b27c8fa736ed90698d90e9b6ef3b94a4802d5e
vihanga-guna <mdvcgunathilaka@gmail.com>
2023-02-01 15:22:33 +0200

    contactus

--- layout/header.php
+++ layout/header.php
@@ -18,14 +18,14 @@
     <div class="row">
   <nav class="navbar navbar-expand-lg" style="background-color: rgb(65, 7, 109);" >
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">ANURADHA</a>
+    <a class="navbar-brand" href="#">Vihanga</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="index.php">Home</a>
+          <a class="nav-link" aria-current="page" href="index.php">Index</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="ex1.php">Exercise1</a>
