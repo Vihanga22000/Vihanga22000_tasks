4e1075b2b37915003740d5e13948d4a06b5458c4
vihanga-guna <mdvcgunathilaka@gmail.com>
2023-02-20 11:12:39 +0200

    2/20

--- layout/ex1.php
+++ layout/ex1.php
@@ -15,6 +15,15 @@ echo "team 7";
 ?>
 <h1>Current Date</h1>
 <?php echo date("d.m.Y")?>
+</br>
+<script>
+document.write("Hello this is JavaScript");
+</script> 
+
+<noscript>
+Please enable JavaScript to view this content
+</noscript>
+<input type="button" onclick="hello()" value="Click me">
 
 
 <h3>3.4 Variable in PHP</h3>
@@ -23,7 +32,23 @@ $title1 = "PHP is interesting";
 echo "<h1>" . $title1 . "</h1>";
 ?>
 
+<script>
+    // //window.alert
+    // window.alert("This will trigger an alert box");
+    // window.alert(5+10);
+    document.write("Hello this is interesting");
+</script>
+
 <h3>3.5 Table & Variable</h3>
+<button onclick="add()">Click to Add</button>
+<hr>
+<p id ="place1" style="color:red; background-color:yellow;"> </P>
+<span id ="place2" style="color:red; background-color:yellow;"></span>
+<script>
+    document.getElementById("place1").innerHTML = "This will go to place1";
+    document.getElementById("place2").innerHTML = "This will go to span
+    ";
+</script>
 <?php
 $g1 =5;
 $g2 =4;
@@ -49,5 +74,9 @@ echo"
 ?>
 <h3> 4 Screenshot of the development environment</h3>
 <img src="ss1.png"alt="">
+<h2>Changing background-color</h2>
+<form action="">
+    <input type="color" name="background" onchange="changeColor('background',this.value)">
+</form>
 <?php include "footer.php"?>
 
---
+++ layout/js/site.js
@@ -0,0 +1,23 @@
+//a simple function that triggers alert box
+function hello(){
+    alert("Welcome to my website");
+}
+//a simple funcion to add 2 numbers
+function add(){
+    document.write(5+6);
+}
+
+
+//display or like echo
+//document.write
+//windows.alert
+//inner.html
+//CONSOLE.LOG
+
+//function to change the background color of the page
+function changeColor(where,newColor){
+    if (where == "background"){
+        document.body.style.backgroundColor = newColor;
+
+    }
+}
